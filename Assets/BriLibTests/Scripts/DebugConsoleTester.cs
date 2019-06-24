77a1857a8f99ad07bb7c24d054ac13a6ee1af2e9
superdeath <cybergen@gmail.com>
2019-06-24 13:56:45 -0700

    Some better test for debug console

---
+++ Assets/BriLibTests/Scripts/DebugConsoleTester.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using BriLib;
+using System;
+using System.Collections.Generic;
+
+public class DebugConsoleTester : MonoBehaviour
+{
+  private void Start()
+  {
+    DebugConsole.Instance.AddCommand("param_list", "List provided params", ParamListCmd);
+  }
+
+  private string ParamListCmd(List<string> args)
+  {
+    var s = String.Empty;
+    foreach (var c in args)
+    {
+      s += c + ", ";
+    }
+    return s;
+  }
+}
