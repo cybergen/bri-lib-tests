28c222ef0f7a8af043650b177e2155f9319b6df3
superdeath <cybergen@gmail.com>
2019-06-24 01:07:40 -0700

    Updated again

---
+++ Assets/BriLibTests/Scripts/PrefsWriter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Threading.Tasks;
+using BriLib;
+
+public class PrefsWriter : MonoBehaviour
+{
+  async void Start()
+  {
+    await Task.Delay(5000);
+    PreferencesManager.Instance.SetBool("WritesCorrectly", true);
+    LogManager.Info("Wrote correctly: " + PreferencesManager.Instance.GetBool("WritesCorrectly"));
+  }
+}
