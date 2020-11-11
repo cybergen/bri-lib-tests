1f69a056c6414029323d2f339b4b11c51e8e376f
brian <cybergen@gmail.com>
2020-11-10 22:01:23 -0800

    Updated ref to lib. Added a themed dialog template. Added in tests for dialog manager in UI scene

---
+++ Assets/BriLibTests/Scripts/DialogSpawner.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using BriLib;
+
+public class DialogSpawner : Panel
+{
+  public GameObject Template;
+
+  public void SpawnDialog()
+  {
+    DialogManager.Instance.ShowDialog(Template, "Title", "Body", () => DialogManager.Instance.HideDialog(Template), new ButtonData("Hide", null, true ));
+  }
+}
